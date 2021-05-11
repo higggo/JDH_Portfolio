7aad047a8272b5db51d298a7cf072ad4be95038b
higggo <higggo@nate.com>
2021-05-11 12:09:13 +0900

    firestore Auth

--- RTD/Assets/Scripts/Server/Firestore.cs
+++ RTD/Assets/Scripts/Server/Firestore.cs
@@ -6,6 +6,7 @@ using Firebase.Firestore;
 using Firebase.Extensions;
 public class Firestore : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -65,5 +66,26 @@ public class Firestore : MonoBehaviour
              uint _money = uint.Parse(result.Value.ToString());
          }
      });
+
+
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        Firebase.Auth.Credential credential =
+    Firebase.Auth.GoogleAuthProvider.GetCredential(googleIdToken, googleAccessToken);
+        auth.SignInWithCredentialAsync(credential).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithCredentialAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
     }
 }
