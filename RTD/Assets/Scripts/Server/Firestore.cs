861f0f96124d03c5e9dcad41c6f715a498cdfb8b
higggo <80950759+CYH1215@users.noreply.github.com>
2021-05-11 14:50:05 +0900

    s

--- RTD/Assets/Scripts/Server/Firestore.cs
+++ RTD/Assets/Scripts/Server/Firestore.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using Firebase.Database;
-using Firebase.Firestore;
-using Firebase.Extensions;
 using Firebase.Auth;
 public class Firestore : MonoBehaviour
 {
@@ -17,64 +14,63 @@ public class Firestore : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
-        CollectionReference citiesRef = db.Collection("cities");
-        citiesRef.Document("SF").SetAsync(new Dictionary<string, object>(){
-        { "Name", "San Francisco" },
-        { "State", "CA" },
-        { "Country", "USA" },
-        { "Capital", false },
-        { "Population", 860000 }
-    }).ContinueWithOnMainThread(task =>
-        citiesRef.Document("LA").SetAsync(new Dictionary<string, object>(){
-            { "Name", "Los Angeles" },
-            { "State", "CA" },
-            { "Country", "USA" },
-            { "Capital", false },
-            { "Population", 3900000 }
-        })
-        ).ContinueWithOnMainThread(task =>
-            citiesRef.Document("DC").SetAsync(new Dictionary<string, object>(){
-            { "Name", "Washington D.C." },
-            { "State", null },
-            { "Country", "USA" },
-            { "Capital", true },
-            { "Population", 680000 }
-            })
-        ).ContinueWithOnMainThread(task =>
-            citiesRef.Document("TOK").SetAsync(new Dictionary<string, object>(){
-            { "Name", "Tokyo" },
-            { "State", null },
-            { "Country", "Japan" },
-            { "Capital", true },
-            { "Population", 9000000 }
-            })
-        ).ContinueWithOnMainThread(task =>
-            citiesRef.Document("BJ").SetAsync(new Dictionary<string, object>(){
-            { "Name", "Beijing" },
-            { "State", null },
-            { "Country", "China" },
-            { "Capital", true },
-            { "Population", 21500000 }
-            })
-        );
+    //    FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+    //    CollectionReference citiesRef = db.Collection("cities");
+    //    citiesRef.Document("SF").SetAsync(new Dictionary<string, object>(){
+    //    { "Name", "San Francisco" },
+    //    { "State", "CA" },
+    //    { "Country", "USA" },
+    //    { "Capital", false },
+    //    { "Population", 860000 }
+    //}).ContinueWithOnMainThread(task =>
+    //    citiesRef.Document("LA").SetAsync(new Dictionary<string, object>(){
+    //        { "Name", "Los Angeles" },
+    //        { "State", "CA" },
+    //        { "Country", "USA" },
+    //        { "Capital", false },
+    //        { "Population", 3900000 }
+    //    })
+    //    ).ContinueWithOnMainThread(task =>
+    //        citiesRef.Document("DC").SetAsync(new Dictionary<string, object>(){
+    //        { "Name", "Washington D.C." },
+    //        { "State", null },
+    //        { "Country", "USA" },
+    //        { "Capital", true },
+    //        { "Population", 680000 }
+    //        })
+    //    ).ContinueWithOnMainThread(task =>
+    //        citiesRef.Document("TOK").SetAsync(new Dictionary<string, object>(){
+    //        { "Name", "Tokyo" },
+    //        { "State", null },
+    //        { "Country", "Japan" },
+    //        { "Capital", true },
+    //        { "Population", 9000000 }
+    //        })
+    //    ).ContinueWithOnMainThread(task =>
+    //        citiesRef.Document("BJ").SetAsync(new Dictionary<string, object>(){
+    //        { "Name", "Beijing" },
+    //        { "State", null },
+    //        { "Country", "China" },
+    //        { "Capital", true },
+    //        { "Population", 21500000 }
+    //        })
+    //    );
 
-        FirebaseDatabase.DefaultInstance
-     .GetReference("GamePlay/System/Money")
-     .GetValueAsync().ContinueWith(task => {
-         if (task.IsFaulted)
-         {
-             Debug.Log("server connect fail");
-         }
-         else if (task.IsCompleted)
-         {
-             DataSnapshot result = task.Result;
-             Debug.Log(result.Value.ToString());
-             uint _money = uint.Parse(result.Value.ToString());
-         }
-     });
-
-
+    //    FirebaseDatabase.DefaultInstance
+    // .GetReference("GamePlay/System/Money")
+    // .GetValueAsync().ContinueWith(task => {
+    //     if (task.IsFaulted)
+    //     {
+    //         Debug.Log("server connect fail");
+    //     }
+    //     else if (task.IsCompleted)
+    //     {
+    //         DataSnapshot result = task.Result;
+    //         Debug.Log(result.Value.ToString());
+    //         uint _money = uint.Parse(result.Value.ToString());
+    //     }
+    // });
+        
         auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
         Firebase.Auth.Credential credential =
     Firebase.Auth.GoogleAuthProvider.GetCredential(googleIdToken, googleAccessToken);
