4dd95f34dfe3c47858bd013372bed529ac054cca
higggo <80950759+CYH1215@users.noreply.github.com>
2021-05-12 14:50:46 +0900

    sign in page

---
+++ Portfolio/Assets/Scripts/Signin/Signin.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Google;
+
+public class Signin : MonoBehaviour
+{
+    //higggo@nate.com 12345789
+
+    Firebase.Auth.FirebaseAuth auth;
+
+    private string webClientId = "872497828709-71vq1pf3hud03salf2nquc2b3habhno5.apps.googleusercontent.com";
+
+    private GoogleSignInConfiguration configuration;
+
+    public TMPro.TMP_InputField Signin_Email;
+    public TMPro.TMP_InputField Signin_Password;
+
+    public TMPro.TMP_InputField Signup_Email;
+    public TMPro.TMP_InputField Signup_Password;
+    // Start is called before the first frame update
+    void Start()
+    {
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+    }
+    
+    public void SigningIn()
+    {
+        auth.SignInWithEmailAndPasswordAsync(Signin_Email.text, Signin_Password.text).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+    public void SigningUp()
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(Signup_Email.text, Signup_Password.text).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+    public void SigninAnonymous()
+    {
+        auth.SignInAnonymouslyAsync().ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInAnonymouslyAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInAnonymouslyAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+    public void SigninGoogle()
+    {
+        GoogleSignIn.Configuration = configuration;
+        GoogleSignIn.Configuration.UseGameSignIn = false;
+        GoogleSignIn.Configuration.RequestIdToken = true;
+        GoogleSignIn.DefaultInstance.SignIn().ContinueWith(
+          OnAuthenticationFinished);
+
+    }
+    
+    internal void OnAuthenticationFinished(Task<GoogleSignInUser> task)
+    {
+        if (task.IsFaulted)
+        {
+            using (IEnumerator<System.Exception> enumerator =
+                    task.Exception.InnerExceptions.GetEnumerator())
+            {
+                if (enumerator.MoveNext())
+                {
+                    GoogleSignIn.SignInException error =
+                            (GoogleSignIn.SignInException)enumerator.Current;
+                    Debug.Log("Got Error: " + error.Status + " " + error.Message);
+                }
+                else
+                {
+                    Debug.Log("Got Unexpected Exception?!?" + task.Exception);
+                }
+            }
+        }
+        else if (task.IsCanceled)
+        {
+            Debug.Log("Canceled");
+        }
+        else
+        {
+            Debug.Log("Welcome: " + task.Result.DisplayName + "!");
+        }
+    }
+
+}
