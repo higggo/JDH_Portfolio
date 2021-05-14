f132295b6209850b0e499293d82c7c550dbc0001
higggo <80950759+CYH1215@users.noreply.github.com>
2021-05-14 14:50:26 +0900

    move

--- Portfolio/Assets/Scripts/Character/Movement/CharacterMove.cs
+++ Portfolio/Assets/Scripts/Character/Movement/CharacterMove.cs
@@ -1,21 +1,103 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase.Database;
+using Firebase.Auth;
+
+public class User
+{
+    public string username;
+    public Vector3 pos;
+
+    public User()
+    {
+    }
+
+    public User(string username, Vector3 pos)
+    {
+        this.username = username;
+        this.pos = pos;
+    }
+}
+
+public class LeaderboardEntry
+{
+    public string uid;
+    public Vector3 pos;
+
+
+    public LeaderboardEntry()
+    {
+    }
+
+    public LeaderboardEntry(string uid, Vector3 pos)
+    {
+        this.uid = uid;
+        this.pos = pos;
+    }
+
+    public Dictionary<string, object> ToDictionary()
+    {
+        Dictionary<string, object> result = new Dictionary<string, object>();
+        result["uid"] = uid;
+        result["pos"] = pos;
+
+        return result;
+    }
+}
 
 public class CharacterMove : MonoBehaviour
 {
+    DatabaseReference reference;
+    FirebaseAuth auth;
+    FirebaseUser user;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Get the root reference location of the database.
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+        auth = FirebaseAuth.DefaultInstance;
+
+        user = auth.CurrentUser;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            WriteNewScore(user.UserId, transform.position);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+
+        }
         if(Input.GetKeyDown(KeyCode.W))
         {
 
         }
     }
+    private void WriteNewScore(string userId, Vector3 pos)
+    {
+        // Create new entry at /user-scores/$userid/$scoreid and at
+        // /leaderboard/$scoreid simultaneously
+        string key = reference.Child("scores").Push().Key;
+        LeaderboardEntry entry = new LeaderboardEntry(userId, transform.position);
+        Dictionary<string, object> entryValues = entry.ToDictionary();
+
+        Dictionary<string, object> childUpdates = new Dictionary<string, object>();
+        childUpdates["/scores/" + key] = entryValues;
+        childUpdates["/user-scores/" + userId + "/" + key] = entryValues;
+
+        reference.UpdateChildrenAsync(childUpdates);
+    }
+
 }
