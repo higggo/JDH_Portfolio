c9d06f8ca7d57945065dd2f18b7606fd785d5d0b
higggo <80950759+CYH1215@users.noreply.github.com>
2021-05-28 14:48:34 +0900

    create user cs

--- Portfolio/Assets/Scripts/Character/Movement/CharacterMove.cs
+++ Portfolio/Assets/Scripts/Character/Movement/CharacterMove.cs
@@ -4,50 +4,6 @@ using UnityEngine;
 using Firebase.Database;
 using Firebase.Auth;
 
-public class User
-{
-    public string username;
-    public Vector3 pos;
-
-    public User()
-    {
-    }
-
-    public User(string username, Vector3 pos)
-    {
-        this.username = username;
-        this.pos = pos;
-    }
-}
-
-public class LeaderboardEntry
-{
-    public string uid;
-    public Vector3 pos;
-
-
-    public LeaderboardEntry()
-    {
-    }
-
-    public LeaderboardEntry(string uid, Vector3 pos)
-    {
-        this.uid = uid;
-        this.pos = pos;
-    }
-
-    public Dictionary<string, object> ToDictionary()
-    {
-       Dictionary<string, object> result = new Dictionary<string, object>();
-       //result["uid"] = uid;
-       result["x"] = pos.x;
-       result["y"] = pos.y;
-       result["z"] = pos.z;
-
-        return result;
-    }
-}
-
 public class CharacterMove : MonoBehaviour
 {
     DatabaseReference reference;
@@ -68,10 +24,6 @@ public class CharacterMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            SendCharactorPos(user.UserId, transform.position);
-        }
         if (Input.GetKey(KeyCode.D))
         {
             Vector3 deltaPos = Vector3.zero;
@@ -93,30 +45,11 @@ public class CharacterMove : MonoBehaviour
 
         }
     }
-    private void SendCharactorPos(string userId, Vector3 pos)
-    {
-        // Create new entry at /user-scores/$userid/$scoreid and at
-        // /leaderboard/$scoreid simultaneously
-        string key = reference.Child("scores").Push().Key;
-        LeaderboardEntry entry = new LeaderboardEntry(userId, transform.position);
-        Dictionary<string, object> entryValues = entry.ToDictionary();
-
-        Dictionary<string, object> childUpdates = new Dictionary<string, object>();
-       // childUpdates["/scores/" + key] = entryValues;
-        childUpdates["/CharacterInfo/" + userId + "/pos"] = entryValues;
-        //childUpdates["/user-scores/" + userId + "/" + key] = entryValues;
-
-        reference.UpdateChildrenAsync(childUpdates);
-
-    }
-
     private void TakeCharacterPos()
     {
         FirebaseDatabase.DefaultInstance
         .GetReference("users")
         .ValueChanged += HandleValueChanged;
-
-
     }
 
     void HandleValueChanged(object sender, ValueChangedEventArgs args)
@@ -138,7 +71,6 @@ public class CharacterMove : MonoBehaviour
 
     public void PushData(Vector3 delta)
     {
-        
         UnityEngine.Assertions.Assert.IsNotNull(reference, "Database ref is null!");
         User temp = new User(name, transform.position + delta);
         string json = JsonUtility.ToJson(temp);        
---
+++ Portfolio/Assets/Scripts/User.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User : MonoBehaviour
+{
+    public string username;
+    public string uid;
+    public Vector3 pos;
+
+    public User()
+    {
+    }
+
+    public User(string username, Vector3 pos)
+    {
+        this.username = username;
+        this.pos = pos;
+    }
+}
