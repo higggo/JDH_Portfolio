01ff5594adf27768099d37e19643ed2012b2ba3f
higggo <80950759+CYH1215@users.noreply.github.com>
2021-05-31 14:51:58 +0900

    create user info

--- Portfolio/Assets/Scripts/Server/CreateCharacters.cs
+++ Portfolio/Assets/Scripts/Server/CreateCharacters.cs
@@ -4,40 +4,52 @@ using UnityEngine;
 using System.IO;
 using Firebase.Database;
 using Firebase.Auth;
+using Firebase.Firestore;
+using Firebase.Extensions;
+public class CharacterCollection
+{
+    public GameObject Character;
+    public string path;
+}
 public class CreateCharacters : MonoBehaviour
 {
     DatabaseReference reference;
     FirebaseAuth auth;
     FirebaseUser user;
+    FirebaseFirestore db;
 
-    public Transform Collection;
+    public Transform CollectionPool;
+    public TMPro.TMP_InputField ID_Inputfield;
 
-    int cnt = 0;
+    public List<CharacterCollection> CollectionCharacters;
+
+    int selectNum = 0;
     // Start is called before the first frame update
     void Start()
     {
-        // Get the root reference location of the database.
+        CollectionCharacters = new List<CharacterCollection>();
         reference = FirebaseDatabase.DefaultInstance.RootReference;
+        db = FirebaseFirestore.DefaultInstance;
         auth = FirebaseAuth.DefaultInstance;
 
-        if (Collection == null) Collection = GameObject.Find("Collection").transform;
-        //Instantiate(Resources.Load("BasicCharacters/BowHitman"), Collection);
+        if (CollectionPool == null) CollectionPool = GameObject.Find("Collection").transform;
+        if (ID_Inputfield == null) ID_Inputfield = GameObject.Find("ID_InputField").GetComponent<TMPro.TMP_InputField>();
 
         DirectoryInfo dir = new DirectoryInfo("Assets/Resources/BasicCharacters");
         FileInfo[] info = dir.GetFiles("*.prefab");
 
+        GameObject obj;
+        string path;
         foreach (FileInfo f in info)
         {
-            Debug.Log(f.Name);
-            
-            Instantiate(Resources.Load("BasicCharacters/"+ f.Name.Replace(".prefab", "")), Collection);
+            CharacterCollection c = new CharacterCollection();
+            path = "BasicCharacters/" + f.Name.Replace(".prefab", "");
+            c.Character = Instantiate(Resources.Load(path), CollectionPool) as GameObject;
+            c.Character.SetActive(false);
+            c.path = path;
+            CollectionCharacters.Add(c);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        CollectionCharacters[selectNum].Character.SetActive(true);
     }
 
     public void OnApplicationQuit()
@@ -50,4 +62,37 @@ public class CreateCharacters : MonoBehaviour
     {
         reference.Child("users").Child(user.UserId).RemoveValueAsync();
     } 
+
+    public void NextCharacter()
+    {
+        CollectionCharacters[selectNum].Character.SetActive(false);
+        selectNum++;
+        if(selectNum > CollectionCharacters.Count - 1)
+            selectNum = 0;
+        CollectionCharacters[selectNum].Character.SetActive(true);
+    }
+
+    public void PreCharacter()
+    {
+        CollectionCharacters[selectNum].Character.SetActive(false);
+        selectNum--;
+        if (selectNum < 0)
+            selectNum = CollectionCharacters.Count-1;
+        CollectionCharacters[selectNum].Character.SetActive(true);
+    }
+
+    public void SelectCharacter()
+    {
+            DocumentReference docRef = db.Collection("user").Document("info");
+            Dictionary<string, object> userInfo = new Dictionary<string, object>
+             {
+                { "CharacterPath", CollectionCharacters[selectNum].path },
+                { "uid", user.UserId },
+                { "cid", ID_Inputfield.text }
+            };
+
+             docRef.SetAsync(userInfo).ContinueWithOnMainThread(task => {
+                Debug.Log("Create User Characater");
+            });
+    }
 }
